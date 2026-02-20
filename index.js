cfbd82aefc77a1b549de110a5ceb999d034667a5
Lucas Sarasa <lucasmsarasa@gmail.com>
2026-02-20 14:57:24 -0300

    feat: add GetUserByIdController and validations

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import 'dotenv/config.js'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
+import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/api/users', async (request, response) => {
+app.post('/api/users', async (req, res) => {
     const createUserController = new CreateUserController()
 
-    const { statusCode, body } = await createUserController.execute(request)
+    const { statusCode, body } = await createUserController.execute(req)
 
-    response.status(statusCode).send(body)
+    res.status(statusCode).send(body)
+})
+
+app.get('/api/users/:userId', async (req, res) => {
+    const getUserByIdController = new GetUserByIdController()
+    const { statusCode, body } = await getUserByIdController.execute(req)
+    res.status(statusCode).send(body)
 })
 
 app.listen(process.env.PORT, () =>
--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,22 +1,29 @@
-export const badRequest = (body) => {
-    return {
-        statusCode: 400,
-        body,
-    }
-}
+// posso fazer assim, ou seja, sem o return e as chaves, porque é um objeto literal, e o js entende que o que vem depois da seta é o retorno da função
+// export const badRequest = (body) => {
+//     return {
+//         statusCode: 400,
+//         body,
+//     }
+// }
 
-export const created = (body) => {
-    return {
-        statusCode: 201,
-        body,
-    }
-}
+export const badRequest = (body) => ({
+    statusCode: 400,
+    body,
+})
 
-export const serverError = () => {
-    return {
-        statusCode: 500,
-        body: {
-            errorMessage: 'Internal Server Error',
-        },
-    }
-}
+export const created = (body) => ({
+    statusCode: 201,
+    body,
+})
+
+export const serverError = () => ({
+    statusCode: 500,
+    body: {
+        errorMessage: 'Internal Server Error',
+    },
+})
+
+export const ok = (body) => ({
+    statusCode: 200,
+    body,
+})
