ddf8cbdef435f48e210cfa473f733d8eb6f28331
Lucas Sarasa <lucasmsarasa@gmail.com>
2026-02-23 18:14:18 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,44 @@
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-emai'
+import { EmailAlreadyInUseError } from '../errors/user'
+import bcrypt from 'bcrypt'
+import PostgresUpdateUserRepository from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        // 1. se o e-mail estiver sendo atualizado, verificar se o novo e-mail já existe para outro usuário
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        // 2. se a senha estiver sendo atualizada, criptografar a nova senha
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+            user.password = hashedPassword
+        }
+
+        // 3. chamar o repository para atualizar o usuário no banco de dados
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updatedUser
+    }
+}
